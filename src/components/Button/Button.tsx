8d467fcb1975f695205f67965ae1a9653876e1b4
Andressa Machado <105513933+AndressaMac@users.noreply.github.com>
2024-01-17 14:19:38 -0300

    feat: vinculo backend

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ButtonLogin } from '../../pages/Login/styles';
 //import { ButtonLogin } from './Button.styles';
 
-<<<<<<< HEAD
+
 type ButtonProps = React.ComponentProps<'button'> & {
     children?: React.ReactNode,
     style?: React.CSSProperties;
@@ -20,35 +20,4 @@ const Button = ({ children, ...props}: ButtonProps) => {
 }
 
 export default Button
-=======
-export type ButtonProps = {
-  children?: React.ReactNode;
-  disabled?: boolean;
-  color?: "primary" | "secondary";
-  title: string;
-  style?: React.CSSProperties;
-  type?: "button" | "submit" | "reset";
-};
-
-export const Button = ({
-  children,
-  disabled,
-  color,
-  title,
-  style,
-  type,
-}: ButtonProps) => {
-  return (
-    <Container
-      title={title}
-      color={color}
-      disabled={disabled}
-      style={style}
-      type={type}
-    >
-      {children}
-    </Container>
-  );
-};
->>>>>>> 8a00cd84f6fc7520807ec1121dfe0b00aedcfc23
 
